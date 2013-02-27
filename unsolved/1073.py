db6d3f2f8a479af97ba589e7d609722ce051eed9
greg <greg@bivc-r402-1.ihep.su>
2013-02-27 13:58:21 +0400

    lagrange algo

--- unsolved/1073.py
+++ unsolved/1073.py
@@ -2,8 +2,15 @@
 
 import math
 N = input()
-i = 0
-while N > 0:
-    N -= (math.sqrt(N) // 1) ** 2
-    i += 1
-print i
+i = [0,0,0,0]
+a = int(math.sqrt(N))
+m = 5
+for i[0] in xrange(a):
+    for i[1] in xrange(a):
+        for i[2] in xrange(a):
+            for i[3] in xrange(a):
+                if i[0]**2 + i[1]**2 + i[2]**2 + i[3]**2 == N:
+                    if 4 - i.count(0) < m:
+                        m = 4 - i.count(0)
+
+print m
