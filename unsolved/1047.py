9712830860b392c29e03ba55048b6276747e8401
greg <greg@bivc-r402-1.ihep.su>
2013-02-28 11:56:52 +0400

    test solution

--- unsolved/1047.py
+++ unsolved/1047.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python
 
-import sys
+import sys,numpy
+def MatToTriang(A):
+    for i in xrange(1,len(A)):
+        k = A[i][i - 1] / (A[i - 1][i - 1] + 0.0)
+        for j in xrange(i - 1,len(A)):
+            A[i][j] -= k * A[i - 1][j]
+    return A
 
 def Minor(A,i,j):
     B = []
@@ -8,9 +14,14 @@ def Minor(A,i,j):
         B.append(z[0:j] + z[j + 1:])
     return B
 
-def MatDet(A):
+def MatDet(A,triangle=False):
     if len(A) == 1:
         return A[0][0]
+    if triangle:
+        t = 1
+        for i in xrange(len(A)):
+            t *= A[i][i]
+        return t
     if len(A) == 2:
         return A[0][0] * A[1][1] - A[0][1] * A[1][0]
     D = 0
@@ -28,15 +39,33 @@ def MatMul(A,B):
 def Transpose(A):
     return map(list,zip(*A))
 
-def MatInv(A):
-    D = MatDet(A)
+def MatInv(A,triangle=False):
+    D = MatDet(A,triangle)
     if D == 0:
         return None
     M = [[((-1)**(i + j)) * MatDet(Minor(A,i,j))/(0.0 + D) for j in xrange(len(A))] for i in xrange(len(A))]
     return Transpose(M)
     
+A = [
+[1,0,0,0],
+[-1,2,1,0],
+[0,-1,2,1],
+[0,0,0,1]
+]
+B = MatToTriang([
+[1,0,0,0],
+[-1,2,1,0],
+[0,-1,2,1],
+[0,0,0,1]
+])
+print numpy.matrix(A)
+print numpy.matrix(B)
+print MatDet(A), MatDet(B,True)
 
-#sys.stdin = open("1047.txt", "r")
+A = []
+B = []
+
+sys.stdin = open("1047.txt", "r")
 N = int(sys.stdin.readline().rstrip())
 a0 = float(sys.stdin.readline().rstrip())
 an1 = float(sys.stdin.readline().rstrip())
@@ -53,6 +82,8 @@ for i in xrange(1,N + 1):
     A[i][i - 1] = -0.5
     A[i][i] = 1
     A[i][i + 1] = -0.5 
-X = MatMul(MatInv(A) , B)
+A = MatToTriang(A)
+print numpy.matrix(A)
+X = MatMul(MatInv(A,True) , B)
 print  "%.2f" % X[1][0]
 
