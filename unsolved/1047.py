d76c99f81e568cfa82b7aad70d148b5b190ad113
greg <greg@bivc-r402-1.ihep.su>
2013-03-01 10:13:00 +0400

    red mem

--- unsolved/1047.py
+++ unsolved/1047.py
@@ -7,7 +7,8 @@ def MatToTriang():
     global B
     for i in xrange(len(A) - 2,-1,-1):
         k = A[i][i + 1] / (A[i + 1][i+1] + 0.0)
-        B[i][0] -= k * B[i+1][0]
+        #B[i][0] -= k * B[i+1][0]
+        B[i] -= k * B[i+1]
         for j in xrange(i - 1, i+2): 
             A[i][j] -= k * A[i+1][j] 
     #return A
@@ -18,12 +19,12 @@ a0 = float(sys.stdin.readline().rstrip())
 an1 = float(sys.stdin.readline().rstrip())
 #a = [a0] + [0]*N + [an1]
 #B initialize
-B = [[float(sys.stdin.readline().rstrip()) * (-2)]   for x in xrange(0,N)]
+B = [float(sys.stdin.readline().rstrip()) * (-2)   for x in xrange(0,N)]
 #B stop
 #c = [float(sys.stdin.readline().rstrip()) * (-2)   for x in xrange(0,N)]
 A = [[ 0 for j in xrange(N)] for i in xrange(N)]
-B[0][0] += a0
-B[N-1][0] += an1
+B[0] += a0
+B[N-1] += an1
 #c[0] += a0
 #c[N - 1] += an1
 #B = [[c[i]] for i in xrange(len(c))]
@@ -39,4 +40,4 @@ for i in xrange(0,N):
     except:
         pass
 MatToTriang()
-print "%.2f" % (B[0][0] / (0.0 + A[0][0]))
+print "%.2f" % (B[0] / (0.0 + A[0][0]))
