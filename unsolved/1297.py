c18cb79caa6d12cc9d4711609b164e68cb904857
greg <greg@bivc-r402-1.ihep.su>
2013-03-01 12:55:06 +0400

    test solution

---
+++ unsolved/1297.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+a = "ThesampletextthatcouldbereadedthesameinbothordersArozaupalanalapuazorA"
+a = " " + a + " "
+d = []
+for i in xrange(len(a)):
+    for j in xrange(len(a)):
+        if a[i:j] == a[j-1:i-1:-1] and a[i:j] != "":
+            d.append(a[i:j])
+maxl = max(len(s) for s in d)
+q = []
+for i in d:
+    if len(i) == maxl:
+        q.append(i)
+del d
+idx = []
+for i in q:
+    idx.append(a.index(i))
+print q[idx.index(min(idx))]
