36ece8f8153c969503d4636f5db30e9379358667
greg <greg@bivc-r402-1.ihep.su>
2013-03-04 14:46:18 +0400

    TLE

--- unsolved/1413.py
+++ unsolved/1413.py
@@ -57,44 +57,25 @@ def Go(num,mode=None):
 
 Mx = [[ Go(i,"x") + Go(j,"x") for j in xrange(0,10)] for i in xrange(0,10)]
 My = [[ Go(i,"y") + Go(j,"y") for j in xrange(0,10)] for i in xrange(0,10)]
-
+Mx[0] = [0] * len(Mx[0])
+My[0] = [0] * len(My[0])
 #sys.stdin = open("1413.txt")
 s = raw_input().rstrip()
 x = 0
 y = 0
 i = 0
-while i < len(s):
-    num = int(s[i:i+2])
-    print num
-    if len(str(num)) == 1:
-        Goxy(num)
+N = len(s)
+while i < N:
+    num = s[i:i+2]
+    if len(num) == 1:
+        Goxy(int(num))
         break
-    istep,jstep = str(num)
+    istep,jstep = num
     istep = int(istep)
     jstep = int(jstep)
     x += Mx[istep][jstep]
     y += My[istep][jstep]
     i += 2
-    if str(num).count("0") != 0:
+    if istep == 0 or jstep == 0:
         break
-##    if num == 8:
-##        y += 1
-##    elif num == 2:
-##        y -= 1
-##    elif num == 6:
-##        x += 1
-##    elif num == 4:
-##        x -= 1
-##    elif num == 9:
-##        x += sqr2
-##        y += sqr2
-##    elif num == 7:
-##        x -= sqr2
-##        y += sqr2
-##    elif num == 3:
-##        x += sqr2
-##        y -= sqr2
-##    elif num == 1:
-##        x -= sqr2
-##        y -= sqr2
 print "%.10f %.10f" % (x,y)
