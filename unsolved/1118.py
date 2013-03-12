df5283444bf0e4884eb8d34c0228340f1dadd811
greg <greg@bivc-r402-1.ihep.su>
2013-03-12 09:54:38 +0400

    TLE

--- unsolved/1118.py
+++ unsolved/1118.py
@@ -1,35 +1,63 @@
 #!/usr/bin/python
-
-def getDel(N):
+import math
+def getTriv(N):
     ans = []
     for i in xrange(1,N // 2 + 1):
         if N % i == 0:
             ans.append(i)
-    return ans
-    
-def getTriv(N):
-    ans = getDel(N)
     return sum(ans) / (0.0 + N)
 
+def getDelit(N):
+    ans = [1]
+    M = N
+    m = N // 2 + 1#int(math.sqrt(N)) + 1
+    i = 2
+    while 2 <= i <= m:
+        while M % i == 0:
+            ans.append(i)
+            M = M / i
+        i += 1
+    ans = expand(ans,N)
+    if M == 1:
+        return ans
+    elif M != N:
+        return ans + [M]
+    else:
+        return ans
+    
+def expand(d,N):
+    d = list(set(d))
+    #print d
+    l = len(d)
+    a = d[:]
+    for i in xrange(1,l):
+        for j in xrange(i,l):
+            if N % (d[i] * d[j]) == 0 and d[i] * d[j] < N:
+                a.append(d[i] * d[j])
+    return list(set(a))
+        
 (I, J) = raw_input().rstrip().split(" ")
 I = int(I)
 J = int(J)
 minn = None
-mint = getTriv(I)
-if I % 2 == 0 and J - I >= 2:
-    step = 2
-    start = I + 1
-elif I % 2 != 0 and J - I >=2:
-    start = I
-    step = 2
-else:
-    step = 1
-    start = I
-    
-for i in xrange(start,J + 1,step):
-    temp = getTriv(i)
-    #print i, temp
-    if temp <= mint:
-        mint = temp
+mint = 10000
+i = J
+step = -1
+while I <= i <= J: 
+    if I == 1:
+        minn = 1
+        break
+    delit = getDelit(i)
+    if len(delit) == 1:
+        if (1 / (1.0 + i) <= mint):
+            minn = i
+            break
+    triv = sum(delit) / (0.0 + i)
+    #print i, delit,triv
+    if triv <= mint:
+        mint = triv
         minn = i
+    if i % 2 != 0 and step != -2:
+        step = -2
+    i += step
 print minn
