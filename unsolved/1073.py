ec8fb7e34a98ef25af0969ce76e0bf2903031f6d
greg <greg@bivc-r402-1.ihep.su>
2013-03-12 15:40:44 +0400

    optimize algo

--- unsolved/1073.py
+++ unsolved/1073.py
@@ -3,26 +3,28 @@
 import sys,math
 
 def getL(n):
+    if int(math.sqrt(N)) ** 2 == N:
+        return 1
     stop = int(math.sqrt(n)) + 1
-    m = []
-    i = [0] * 4
+    m = 0
+    i = [0] * 3
     for i[0] in xrange(stop):
-        if i[0] ** 2 == N:
-            m.append(1)
+##        if i[0] ** 2 == n:
+##            m.append(1)
         for i[1] in xrange(i[0],stop):
-            if sum([i[x] ** 2 for x in xrange(2)]) == N:
-                m.append(2)
+            if sum([i[x] ** 2 for x in xrange(2)]) == n:
+                return 2
+##                m.append(2)
             for i[2] in xrange(i[1],stop):
-                if sum([i[x] ** 2 for x in xrange(3)])  == N:
-                    m.append(3)
-                for i[3] in xrange(i[2],stop):
-                    if sum([i[x] ** 2 for x in xrange(4)])  == N:
-                        m.append(4)
-                    #print i, sum([i[x] ** 2 for x in xrange(4)])
-    return min(m)
-
-
-
+                if sum([i[x] ** 2 for x in xrange(3)])  == n:
+                    ##m.append(3)
+                    m = 1
+    if m == 1:
+        return 3
+    else:
+        return 4
 
 N = input()
+d = []
+
 print getL(N)
