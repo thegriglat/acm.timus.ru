f63375c01dc6d362f8d9c45d7d58e5449cabf7cd
greg <greg@bivc-r402-1.ihep.su>
2013-03-12 16:50:23 +0400

    speed increate TLE 9

--- unsolved/1073.py
+++ unsolved/1073.py
@@ -3,24 +3,27 @@
 import sys,math
 
 def getL(n):
-    if int(math.sqrt(N)) ** 2 == N:
+    """
+    Decomposition of n by Lagrange theorem.
+    n = a**2 + b**2 + c**2 + d**2
+    """
+    if int(math.sqrt(n)) ** 2 == N:
         return 1
     i = 0
     a = 5
-    while int(math.sqrt(N)) - i > 0:
-        a = int(math.sqrt(N)) - i
-        b = math.sqrt(N - a ** 2)
+    while a > 0:
+        a = int(math.sqrt(n)) - i
+        b = math.sqrt(n - a ** 2)
         #print a ** 2, b**2
         if int(b) == b:
             return 2
         i += 1
-  
     stop = int(math.sqrt(n)) + 1
     m = 0
     i = [0] * 3
-    for i[0] in xrange(stop):
-        for i[1] in xrange(i[0],stop):
-            for i[2] in xrange(i[1],stop):
+    for i[0] in xrange(stop,0,-1):
+        for i[1] in xrange(1,i[0]):
+            for i[2] in xrange(stop):
                 if sum([i[x] ** 2 for x in xrange(3)])  == n:
                     return 3
     return 4
