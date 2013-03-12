e14307b59e96e6468edb69dc31d191c5d4814fcd
greg <greg@bivc-r402-1.ihep.su>
2013-03-12 16:11:14 +0400

    spped up, but WA

--- unsolved/1073.py
+++ unsolved/1073.py
@@ -5,26 +5,22 @@ import sys,math
 def getL(n):
     if int(math.sqrt(N)) ** 2 == N:
         return 1
+    i = 0
+    while int(math.sqrt(N)) - i > 0:
+        b = math.sqrt(N - int(math.sqrt(N)) ** 2)
+        if int(b) == b:
+            return 2
+        i += 1
+  
     stop = int(math.sqrt(n)) + 1
     m = 0
     i = [0] * 3
     for i[0] in xrange(stop):
-##        if i[0] ** 2 == n:
-##            m.append(1)
         for i[1] in xrange(i[0],stop):
-            if sum([i[x] ** 2 for x in xrange(2)]) == n:
-                return 2
-##                m.append(2)
             for i[2] in xrange(i[1],stop):
                 if sum([i[x] ** 2 for x in xrange(3)])  == n:
-                    ##m.append(3)
-                    m = 1
-    if m == 1:
-        return 3
-    else:
-        return 4
+                    return 3
+    return 4
 
 N = input()
-d = []
-
 print getL(N)
