755f7d5d4c3f8444f332cbbb7313ece6e561b0d9
greg <greg@bivc-r402-1.ihep.su>
2013-03-13 12:09:46 +0400

    AC

--- unsolved/1073.py
+++ unsolved/1073.py
@@ -14,19 +14,25 @@ def getL(n):
     while a > 0:
         a = int(math.sqrt(n)) - i
         b = math.sqrt(n - a ** 2)
-        #print a ** 2, b**2
+        if a < b: break     #Optimize 2x
+        #print a, b
         if int(b) == b:
             return 2
         i += 1
     stop = int(math.sqrt(n)) + 1
     m = 0
     i = [0] * 3
-    for i[0] in xrange(stop,0,-1):
-        for i[1] in xrange(1,i[0]):
-            for i[2] in xrange(stop):
+    speed = 3
+    if N < 81:
+        speed = 1
+    for i[0] in xrange(1,stop // speed):
+        for i[1] in xrange(i[0],stop // speed):
+            for i[2] in xrange(i[1],stop):
+                #print i
                 if sum([i[x] ** 2 for x in xrange(3)])  == n:
                     return 3
     return 4
 
 N = input()
 print getL(N)
+
